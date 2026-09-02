4758c8f1d94d11823c84e4c6740c85a236ba20f6
Clay Good <hi@claygood.com>
2026-09-02 10:35:52 -0500

    fix(nix): invalidate pnpm dependency hash

--- flake.nix
+++ flake.nix
@@ -52,7 +52,7 @@
               inherit (finalAttrs) pname version src;
               pnpm = pkgs.pnpm_10;
               fetcherVersion = 3;
-              hash = "sha256-+qGFLSVLJ9faZOmfO6ZVBP525i5LRgwhsJat2vT7Aw8=";
+              hash = "";
             };
 
             nativeBuildInputs = with pkgs; [
