ff45328f44a95e9782cea410384b3a15f82d54db
Clay Good <hi@claygood.com>
2026-09-21 09:29:56 -0500

    chore(nix): update pnpm dependency hash
    Matches the Inquirer 12 lockfile update and fixes Nix Flake Validation.

--- flake.nix
+++ flake.nix
@@ -52,7 +52,7 @@
               inherit (finalAttrs) pname version src;
               pnpm = pkgs.pnpm_10;
               fetcherVersion = 3;
-              hash = "sha256-oz4tsfu05IPDMaBBp5jLbfsxvTmw1oVtNFtpvudCOPE=";
+              hash = "sha256-C5uUzDTDJICJciPn9IUXGjWc2Gt4ObmAHyKjnncnyV8=";
             };
 
             nativeBuildInputs = with pkgs; [
